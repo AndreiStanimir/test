823f4f4fb0c83910ff0d78b57d23ab677888a732
Andrei Stănimir <aandrei.stanimir@gmail.com>
2019-05-27 10:57:28 +0300

    Add files via upload

---
+++ Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Deck
+    {
+        public List<int> groupsOfCards;
+        int nrPacks;
+        public int numberOfCards=0;
+        Deck(int packs)
+        {
+            groupsOfCards.Add(0);
+            for (int i = 1; i <= 9; i++)
+            {
+                groupsOfCards.Add(packs * 4);
+            }
+            // Add 10s 
+            groupsOfCards.Add(packs * 4 * 4);
+            numberOfCards = packs * 52;
+        }
+
+        void Shuffle()
+        {
+
+        }
+    }
+}
